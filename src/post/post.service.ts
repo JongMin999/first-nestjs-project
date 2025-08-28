08d53c6eee105528be369e5729ce427c935eb918
MalRang <ekaloses@naver.com>
2025-08-28 12:00:00 +0900

    첫 NestJS 프로젝트 생성 & 블로그 포스트 API 구현

---
+++ src/post/post.service.ts
@@ -0,0 +1,28 @@
+import { CreatePostDto } from "./dto/create-post.dto";
+import { UpdatePostDto } from "./dto/update-post.dto";
+import { Post } from "./post.entity"
+
+export class PostService{
+    constructor(private readonly postRepository: PostRepository) {}
+
+    async findAll(): Promise<Post[]> {
+        return this.postRepository.find();
+    }
+
+    async findOne(id: number): Promise<Post | null>{
+        return this.postRepository.findOne({where: { id } });
+    }
+    async create(createPostDto: CreatePostDto): Promise<Post> {
+    const post = this.postRepository.create(createPostDto);
+    return this.postRepository.save(post);
+    }
+
+    async update(id: number, updatePostDto: UpdatePostDto): Promise<Post | null> {
+    await this.postRepository.update(id, updatePostDto);
+    return this.postRepository.findOne({ where: { id } });
+    }
+
+    async remove(id: number): Promise<void> {
+    await this.postRepository.delete(id);
+    }
+}
